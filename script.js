8178903e93e647b70bba67bc647dcde263d02b1e
ArunThankaiah <arunchandhu120@gmail.com>
2024-02-05 12:10:40 +0530

    responsive nav bar

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-const menu = document.querySelector(".menu")
-const menuLine = document.querySelector("nav ul")
-menu.addEventListener('click',()=>{
-    menuLine.classList.toggle('showmenu')
-})
+// const menu = document.querySelector(".menu")
+// const menuLine = document.querySelector("nav ul")
+// menu.addEventListener('click',()=>{
+//     menuLine.classList.toggle('showmenu')
+// })
 
+var toggle = document.getElementById("small-device")
+var smallDevice = document.querySelector('.small-device')
+var closeBtn = document.getElementById("close-btn")
+var smallList = document.querySelector('.small-list')
 
+function showMenu(){
+    smallDevice.style.display = "block"
+    toggle.style.display = "none"
+    closeBtn.style.display = "block"
+    smallList.style.display = "block"
+}
 
+function closeButton(){
+    smallDevice.style.display = "none"
+    toggle.style.display = "block"
+    closeBtn.style.display = "none"
+    smallList.style.display = "none"
+
+}
