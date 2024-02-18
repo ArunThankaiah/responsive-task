e0542f224384ae099768e4c39cab8ad836e7723d
ArunThankaiah <arunchandhu120@gmail.com>
2024-02-18 13:34:37 +0530

    new file

--- script.js
+++ script.js
@@ -4,22 +4,3 @@
 //     menuLine.classList.toggle('showmenu')
 // })
 
-var toggle = document.getElementById("small-device")
-var smallDevice = document.querySelector('.small-device')
-var closeBtn = document.getElementById("close-btn")
-var smallList = document.querySelector('.small-list')
-
-function showMenu(){
-    smallDevice.style.display = "block"
-    toggle.style.display = "none"
-    closeBtn.style.display = "block"
-    smallList.style.display = "block"
-}
-
-function closeButton(){
-    smallDevice.style.display = "none"
-    toggle.style.display = "block"
-    closeBtn.style.display = "none"
-    smallList.style.display = "none"
-
-}
